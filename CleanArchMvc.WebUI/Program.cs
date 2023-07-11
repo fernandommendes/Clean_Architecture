d46c446e4d0f7e896e5b214aff6886ffb0660d9a
Fernando Moreira Mendes <fernando_moreiram@hotmail.com>
2023-07-10 21:43:20 -0300

    Implementação dos projetos de Infra

--- CleanArchMvc.WebUI/Program.cs
+++ CleanArchMvc.WebUI/Program.cs
@@ -1,5 +1,7 @@
-var builder = WebApplication.CreateBuilder(args);
+using CleanArchMvc.Infra.Ioc;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddInfrastructure(builder.Configuration);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
--- CleanArchMvc.WebUI/Startup.cs
+++ CleanArchMvc.WebUI/Startup.cs
@@ -1,13 +1,4 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CleanArchMvc.Infra.Ioc;
 
 namespace CleanArchMvc.WebUI
 {
@@ -23,6 +14,7 @@ namespace CleanArchMvc.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddInfrastructure(Configuration);
             services.AddControllersWithViews();
         }
 
