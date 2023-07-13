fb648331745d19cd3c52e95bea65dfdbe703869d
Fernando Moreira Mendes <fernando_moreiram@hotmail.com>
2023-07-12 22:28:41 -0300

    Implementação do Service de Category

---
+++ CleanArchMvc.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMvc.Application.DTOs;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    internal interface ICategoryService
+    {
+        Task<IEnumerable<CategoryDTO>> GetCategories();
+        Task<CategoryDTO> GetById(int? id);
+        Task Add(CategoryDTO categoryDto);
+        Task Update(CategoryDTO categoryDto);
+        Task Remove(int? id);
+    }
+}
